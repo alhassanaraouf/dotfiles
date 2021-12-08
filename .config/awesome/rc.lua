2cc1d9c659ad747e09d13f07affdf05ac8db38fd
Al-Hassan Abdel-Raouf <alhassanaraouf@disroot.org>
2021-12-09 00:35:29 +0200

    hide awesomewm brightness widget and change default duedate and date format in todoman

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -265,7 +265,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
 	    volume_widget,
-	    brightness_widget(brightness_config),
+	    -- brightness_widget(brightness_config),
 	    cpu_widget(cpu_config), 
             layout = wibox.layout.fixed.horizontal,
 	    kbdcfg.widget,
