b158703673bb35e09f63365e056fa3b8072d4c4b
Al-Hassan Abdel-Raouf <alhassanaraouf@disroot.org>
2021-12-08 13:29:56 +0200

    Config to Use vis as External Editor in Luakit

--- .config/luakit/userconf.lua
+++ .config/luakit/userconf.lua
@@ -1,9 +1,15 @@
+-- Loading NoScript Plugin
 require "noscript"
+
+-- Loading Tabgroups Plugin
 require "plugins/tabgroups"
+
+-- Setup My Search Engine
 local settings = require "settings"
 local engines = settings.window.search_engines
-
 engines.disroot = "https://search.disroot.org/?q=%s"
-
 engines.default = engines.disroot
 
+-- Setup My Editor
+local editor = require "editor"
+editor.editor_cmd = "xterm -e vis {file}"
